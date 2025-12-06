1c4c596c752db7800805d2bb5fbf8ee677c6824f
Zaur Aliyev <zauraliyevturnup@gmail.com>
2025-12-06 12:37:28 +0400

    3rd

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Header from "../src/components/Header";
-import Hero from "../src/components/Hero";
-import Services from "../src/components/Services";
-import Banner from "../src/components/Banner";
-import Footer from "../src/components/Footer";
+// import Header from "../src/components/Header";
+// import Hero from "../src/components/Hero";
+// import Services from "../src/components/Services";
+// import Banner from "../src/components/Banner";
+// import Footer from "../src/components/Footer";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen">
       <img
         src="/Generated Image December 06, 2025 - 11_51AM.jpeg"
-        alt="image"
+        alt="image3"
       />
       <img src="/veo video reference image (9).jpg" alt="image" />
       <img src="/veo video reference image (5).jpg" alt="image 2" />
-      <Header />
+      {/* <Header />
       <Hero />
       <Services />
       <Banner />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
